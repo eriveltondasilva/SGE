8699745c251c9cbe347d7823e691fe5bc91c019a
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-03-30 02:18:28 -0300

    Configuração de migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,14 +17,13 @@ return new class extends Migration
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum(
-                'role',
-                ['admin', 'manager', 'coordinator', 'teacher', 'student']
-                )->nullable();
-                $table->rememberToken();
-                $table->timestamps();
-                $table->boolean('status')->default(true);
-                $table->foreignId('school_id')->nullable();
+            $table->enum('role',
+                ['admin', 'manager', 'coordinator', 'teacher', 'student'])
+                ->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            $table->boolean('status')->default(true);
+            $table->foreignId('school_id')->nullable();
         });
     }
 
