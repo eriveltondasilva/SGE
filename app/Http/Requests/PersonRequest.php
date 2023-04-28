bf50fe881efc6340d91b8b89459e604f3a820853
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-04-28 19:26:09 -0300

    teste

--- app/Http/Requests/PersonRequest.php
+++ app/Http/Requests/PersonRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Student;
+// use App\Models\Relative;
+use App\Models\Teacher;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class PersonRequest extends FormRequest
 {
@@ -23,35 +27,53 @@ class PersonRequest extends FormRequest
     {
         return [
             // dados pessoais do aluno e do professor
-            'name'        => 'bail|required|string|between:4,100',
-            'rg'          => 'string|nullable|size:9',
-            'cpf'         => 'string|nullable|size:14',
-            'email'       => 'email|nullable|max:100',
-            'telephone'   => 'string|nullable|size:16',
-            'birth_date'  => 'date|nullable',
-            'nationality' => 'string|nullable',
-            'gender'      => 'string|nullable|max:1',
+            'name' => ['required', 'string', 'between:4,100'],
+            'rg'   => ['string', 'nullable', 'size:9'],
+            'cpf'  => ['string', 'nullable', 'size:14'],
+
+            // Validação de email do aluno e do professor'
+            'email' => [
+                'bail', 'email', 'nullable', 'max:100',
+
+                Rule::when(request()->routeIs('student.*'), [
+                    Rule::unique(Student::class)->ignore($this->student->id ?? '')
+                ]),
+                Rule::when(request()->routeIs('teacher.*'), [
+                    Rule::unique(Teacher::class)->ignore($this->teacher->id ?? '')
+                ]),
+            ],
+
+            'telephone'   => ['string', 'nullable', 'size:16'],
+            'birth_date'  => ['date', 'nullable', 'before:today'],
+            'nationality' => ['string', 'nullable'],
+            'gender'      => [Rule::in(['F', 'M'])],
+
 
             // dados específicos do aluno
-            'birth_place'     => 'sometimes|string|nullable',
-            'gov_benefits'    => 'sometimes|string|nullable',
-            'health_problems' => 'sometimes|string|nullable',
-            'note'            => 'sometimes|string|nullable',
+            'birth_place'     => ['sometimes', 'string', 'nullable'],
+            'gov_benefits'    => ['sometimes', 'string', 'nullable'],
+            'health_problems' => ['sometimes', 'string', 'nullable'],
+            'note'            => ['sometimes', 'string', 'nullable'],
+
 
             // dados do responsável pelo aluno
-            'relative.name'      => 'string|nullable|between:4,100',
-            'relative.email'     => 'email|nullable|max:100',
-            'relative.telephone' => 'string|nullable|size:16',
-            'relative.kinship'   => 'string|nullable',
+            'relative.name'  => ['string', 'nullable', 'between:4,100'],
+            'relative.email' => ['bail', 'email', 'nullable', 'max:100',
+                // TODO: validar o email do responsável
+                // Rule::unique(Relative::class)->ignore($this->relative->id ?? '')
+            ],
+            'relative.telephone' => ['string', 'nullable', 'size:16'],
+            'relative.kinship'   => ['string', 'nullable'],
+
 
             // dados do endereço do aluno e do professor
-            'address.street'       => 'string|nullable|max:255',
-            'address.complement'   => 'string|nullable|max:255',
-            'address.neighborhood' => 'string|nullable',
-            'address.city'         => 'string|nullable',
-            'address.cep'          => 'string|nullable',
-            'address.state'        => 'string|nullable',
-
+            'address.street'       => ['string', 'nullable'],
+            'address.complement'   => ['string', 'nullable'],
+            'address.neighborhood' => ['string', 'nullable'],
+            'address.city'         => ['string', 'nullable'],
+            'address.cep'          => ['string', 'nullable'],
+            'address.state'        => ['string', 'nullable'],
         ];
     }
+
 }
